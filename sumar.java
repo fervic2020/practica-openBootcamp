0cb9eee1a8f62cb4965a65573921ea933127ed1b
fervic <fervicparedes@gmail.com>
2022-09-05 17:24:53 -0400

    función que se encargar de sumar 3 numeros

---
+++ sumar.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int parametro1 = 1;
+        int parametro2 = 2;
+        int parametro3 = 3;
+        sumar(parametro1, parametro2, parametro3);
+    }
+
+    public static int sumar(int a, int b, int c) {
+        return a + b + c;
+    }
+}
